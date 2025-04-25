470817a0ed3ade7417ca985a98920cbc0db85b8e
Rohit Akurdekar <114896370+RohitAkurdekar@users.noreply.github.com>
2025-04-25 20:02:23 +0530

    Update test.py

--- Edge Computing/mongo/test.py
+++ Edge Computing/mongo/test.py
@@ -1,10 +1,8 @@
 import user
 
-user.insert_user("Rohit Akurdekar","Rohit@1996","rakurdekar@gmail.com",9604268633)
-user.insert_user("Shweta Mahale","Shweta@1997","shweta.mahale.cse19@gmail.com",8329173478)
-user.insert_user("Vrushali Lobhe","Vrushali@1998","vrushalilobhe3050@gmail.com",9022416855)
-user.insert_user("Saurav Pimpalkar","Saurav@1999","s.auravpimpalkar880@gmail.com",8805505023)
-
-# print(user.get_user("shweta.mahale.cse19@gmail.com","Shweta@19297"))
+user.insert_user("Rohit Akurdekar","XXXXXXXXXX","rakurdekar@gmail.com",XXXXXXXXXX)
+user.insert_user("Shweta Mahale","XXXXXXXXXX","shweta.mahale.cse19@gmail.com",XXXXXXXXXX)
+user.insert_user("Vrushali Lobhe","XXXXXXXXXX","vrushalilobhe3050@gmail.com",XXXXXXXXXX)
+user.insert_user("Saurav Pimpalkar","XXXXXXXXXX","s.auravpimpalkar880@gmail.com",XXXXXXXXXX)
 
 
